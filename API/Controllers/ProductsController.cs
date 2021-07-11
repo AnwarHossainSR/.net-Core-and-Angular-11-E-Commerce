1161d4ec66e4f223bf7564ae80f991b32c030d20
AnwarHossainSR <mahedianwar@gmail.com>
2021-07-11 11:30:12 +0600

    with error

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,10 +1,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using API.Data;
-using API.Entities;
-//using Infrastructure.Data;
-//using Core.Entities;
+
+using Infrastructure.Data;
+using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
-using API.Entities;
+using Core.Entities;
 
-namespace API.Data {
+namespace Infrastructure.Data {
     public class StoreContext : DbContext {
         public StoreContext(DbContextOptions<StoreContext> options) : base (options) { }
         public DbSet<Product> Products { get; set; }
